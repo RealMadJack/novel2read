463e63a2021b7543e0911f11aa29d39722238880
Dmitry G <gupalo1108@gmail.com>
2019-03-14 13:55:19 +0300

    Tests BookGenreUrlTest, GenrePageViewTest

--- novel2read/apps/books/tests/test_urls.py
+++ novel2read/apps/books/tests/test_urls.py
@@ -4,13 +4,13 @@ from django.urls import resolve
 
 class BookGenreUrlTest(TestCase):
     def setUp(self):
-        self.resolver_list = resolve('category/')
-        self.resolver_solo = resolve('category/test-genre/')
+        self.resolver_all = resolve('/category/all/')
+        self.resolver_solo = resolve('/category/test-genre/')
 
-    # def test_bookgenre_resolve(self):
-    #     self.assertNotEqual(self.resolver_list.view_name, 'books:genre-all')
-    #     self.assertNotEqual(self.resolver_solo.view_name, 'books:genre')
+    def test_bookgenre_resolve(self):
+        self.assertEqual(self.resolver_all.view_name, 'books:genre-all')
+        self.assertEqual(self.resolver_solo.view_name, 'books:genre')
 
-    # def test_bookgenre_resolve_invalid(self):
-    #     self.assertNotEqual(self.resolver_list.view_name, '')
-    #     self.assertNotEqual(self.resolver_solo.view_name, '')
+    def test_bookgenre_resolve_invalid(self):
+        self.assertNotEqual(self.resolver_all.view_name, '')
+        self.assertNotEqual(self.resolver_solo.view_name, '')
--- novel2read/apps/books/tests/test_views.py
+++ novel2read/apps/books/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.test import Client, TestCase
 from django.urls import reverse
 
+from ..models import BookGenre, Book
+
 
 class FrontPageViewTest(TestCase):
     def setUp(self):
@@ -15,7 +17,32 @@ class FrontPageViewTest(TestCase):
 
     def test_frontpage_content(self):
         self.assertIn('html', self.response.content.decode('utf-8'))
-        # self.assertEqual(self.response.content.decode('utf-8'), {})
 
     def test_frontpage_content_invalid(self):
         self.assertNotEqual(self.response.content.decode('utf-8'), {})
+
+
+class GenrePageViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.bookgenre = BookGenre.objects.create(name='test genre')
+        self.book = Book.objects.create(title='test book', bookgenre=self.bookgenre)
+        self.response_all = self.client.get(reverse('books:genre-all'))
+        self.response_solo = self.client.get(
+            reverse('books:genre', kwargs={'bookgenre_slug': self.bookgenre.slug}))
+
+    def test_frontpage_response(self):
+        self.assertEqual(self.response_all.status_code, 200)
+        self.assertEqual(self.response_solo.status_code, 200)
+
+    def test_frontpageresponse_invalid(self):
+        self.assertNotEqual(self.response_all.status_code, 404)
+        self.assertNotEqual(self.response_solo.status_code, 404)
+
+    def test_frontpage_content(self):
+        self.assertIn('html', self.response_all.content.decode('utf-8'))
+        self.assertIn(self.bookgenre.name, self.response_solo.content.decode('utf-8'))
+
+    def test_frontpage_content_invalid(self):
+        self.assertNotEqual(self.response_all.content.decode('utf-8'), {})
+        self.assertNotEqual(self.response_solo.content.decode('utf-8'), {})
