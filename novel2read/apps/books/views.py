bf7597314fb4e0278ee85c6b429fe62b695a7f13
Dmitry G <gupalo1108@gmail.com>
2019-03-18 14:52:12 +0300

    Bookchapter page added breadcrumbs

--- novel2read/apps/books/views.py
+++ novel2read/apps/books/views.py
@@ -67,7 +67,7 @@ class BookChapterView(DetailView):
 
     def get(self, request, *args, **kwargs):
         try:
-            bookchapter = BookChapter.objects.get(pk=kwargs['bookchapter_pk'])
+            bookchapter = BookChapter.objects.select_related('book').get(pk=kwargs['bookchapter_pk'])
             bookchapters = BookChapter.objects.filter(book__slug=kwargs['book_slug']).select_related('book')
             prev_chap = prev_in_order(bookchapter, qs=bookchapters)
             next_chap = next_in_order(bookchapter, qs=bookchapters)
