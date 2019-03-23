868012a6216a58ee2d683de5ceb17df94730260b
Dmitry G <gupalo1108@gmail.com>
2019-03-23 18:05:37 +0300

    register c_id readonly

--- novel2read/apps/users/models.py
+++ novel2read/apps/users/models.py
@@ -59,9 +59,7 @@ class Library(models.Model):
 class BookProgress(models.Model):
     book = models.OneToOneField(Book, on_delete=models.CASCADE)
     library = models.ForeignKey(Library, on_delete=models.SET_NULL, blank=True, null=True)
-    # bc_id = models.IntegerField('Chapter ID', blank=True, null=True, default=0)
     c_id = models.IntegerField('Chapter ID', blank=True, null=True, default=0)
-    # scroll position
 
     class Meta:
         verbose_name = 'Book Progress'
