219e943f39c12e5b175814e0b61770ec29b141f0
Dmitry G <gupalo1108@gmail.com>
2019-03-24 22:32:50 +0300

    Added swiper.js, front page slider

--- novel2read/static/js/project.js
+++ novel2read/static/js/project.js
@@ -2,6 +2,25 @@
 
 (function () {
 
+  var swiper = new Swiper('.swiper-container', {
+      pagination: '.swiper-pagination',
+      initialSlide: 3,
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      loop: true,
+      slidesPerView: 'auto',
+      coverflow: {
+          rotate: 0,
+          stretch: 0,
+          depth: 250,
+          modifier: 1,
+          slideShadows : true
+      },
+      //preventClicks: false,
+      //preventClicksPropagation: false,
+      slideToClickedSlide: true
+  });
 
 })();
 
