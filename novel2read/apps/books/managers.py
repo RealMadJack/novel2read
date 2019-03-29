226ec9aa78f2194d8d63b9403ae95f29c0e8efe5
Dmitry G <gupalo1108@gmail.com>
2019-03-29 14:16:27 +0300

    removed last commit

--- novel2read/apps/books/managers.py
+++ novel2read/apps/books/managers.py
@@ -2,8 +2,5 @@ from django.db import models
 
 
 class BookManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().filter(status=1)
-
-    # def published(self):
-    #     return self.filter(status=1)
+    def published(self):
+        return self.filter(status=1)
