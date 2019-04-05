507da55bf0ec8f362d25457288595f5aecc17653
Dmitry G <gupalo1108@gmail.com>
2019-04-05 17:57:14 +0300

    front_page slider bugfixing

--- novel2read/static/js/project.js
+++ novel2read/static/js/project.js
@@ -2,24 +2,28 @@
 
 (function () {
 
-  var swiper = new Swiper('.swiper-container', {
-      pagination: '.swiper-pagination',
-      effect: 'coverflow',
-      // grabCursor: true,
-      centeredSlides: true,
-      loop: true,
-      slidesPerView: 'auto',
-      coverflow: {
-          rotate: 0,
-          stretch: 0,
-          depth: 250,
-          modifier: 1,
-          slideShadows : true
-      },
-      //preventClicks: false,
-      //preventClicksPropagation: false,
-      slideToClickedSlide: true,
-  });
+let slides = window.innerWidth > 1400 ? 2 : 'auto'
+
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    effect: 'coverflow',
+    // grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: slides,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    coverflow: {
+        rotate: 0,
+        stretch: 0,
+        depth: 250,
+        modifier: 1,
+        slideShadows : true
+    },
+    //preventClicks: false,
+    //preventClicksPropagation: false,
+    slideToClickedSlide: true,
+});
 
 
 
