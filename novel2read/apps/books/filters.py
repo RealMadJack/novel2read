fd9f0a29e4264cc5d80b51acc43c48a139ad0bb9
Dmitry G <gupalo1108@gmail.com>
2019-04-09 15:47:31 +0300

    BookFilter choices chapters, votes, rating field

--- novel2read/apps/books/filters.py
+++ novel2read/apps/books/filters.py
@@ -5,8 +5,6 @@ import django_filters
 
 class BookFilter(django_filters.FilterSet):
     """
-    country: choices(china, korea, japan) -> instance.country.lower()
-    chapters: chapters min / max
     votes(popular): votes min / max
     rating: rating min / max
     """
@@ -15,8 +13,25 @@ class BookFilter(django_filters.FilterSet):
         ('korea', 'Korea'),
         ('japan', 'Japan'),
     )
+    CHAPTER_FILTER = (
+        ('low', 'Lowest'),
+        ('high', 'Highest'),
+    )
+    VOTES_FILTER = (
+        ('low', 'Lowest'),
+        ('high', 'Highest'),
+    )
+    RATING_FILTER = (
+        (1, 'more than 1 star'),
+        (2, 'more than 2 star'),
+        (3, 'more than 3 star'),
+        (4, 'more than 4 star'),
+    )
 
     country = django_filters.ChoiceFilter(label='Country', choices=COUNTRY_CHOICES, method='filter_by_country')
+    chapters = django_filters.ChoiceFilter(label='Chapters', choices=CHAPTER_FILTER, method='filter_by_chapters')
+    votes = django_filters.ChoiceFilter(label='Votes', choices=VOTES_FILTER, method='filter_by_votes')
+    rating = django_filters.ChoiceFilter(label='Rating', choices=RATING_FILTER, method='filter_by_rating')
 
     class Meta:
         model = Book
@@ -28,4 +43,17 @@ class BookFilter(django_filters.FilterSet):
 
     def filter_by_country(self, qs, name, value):
         qs = qs if not value else qs.filter(country__iexact=value)
+        return qs
+
+    def filter_by_chapters(self, qs, name, value):
+        expression = 'chapters' if value == 'low' else '-chapters'
+        return qs.order_by(expression)
+
+    def filter_by_votes(self, qs, name, value):
+        expression = 'chapters' if value == 'low' else '-chapters'
+        return qs.order_by(expression)
+
+    def filter_by_rating(self, qs, name, value):
+        qs = qs if not value else qs.filter(rating__gte=value)
+        print(qs)
         return qs
