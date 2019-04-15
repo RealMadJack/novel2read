470e86066d8f54937ae1dff2e20842289269920c
Dmitry G <gupalo1108@gmail.com>
2019-04-15 10:10:18 +0300

    update_users_votes

--- novel2read/apps/users/tasks.py
+++ novel2read/apps/users/tasks.py
@@ -1,16 +1,29 @@
+from celery import states
+from celery.exceptions import Ignore
 from novel2read.taskapp.celery import app
 from django.contrib.auth import get_user_model
+from .models import Profile
 
 User = get_user_model()
 
 
 @app.task(bind=True)
 def update_users_votes(self):
-    users = User.objects.all()
-    user_prem = users.filter(profile__premium=True)
-    user_nonprem = users.filter(profile__premium=False)
-    # try:
-    #     user_prem.profile.update(votes=3)
-    #     user_nonprem.profile.update(votes=6)
-    # except Exception as ex:
-    #     raise ex
+    try:
+        profiles = Profile.objects.all()
+        profiles_prem = profiles.filter(premium=True)
+        profiles_nonprem = profiles.filter(premium=False)
+        profiles_prem.update(votes=6)
+        profiles_nonprem.update(votes=3)
+        # users = User.objects.all()
+        # users_prem = users.filter(profile__premium=True)
+        # users_nonprem = users.filter(profile__premium=False)
+        # users_prem.update(profile__votes=3)
+        # users_nonprem.update(profile__votes=6)
+        return 'success'
+    except Exception as ex:
+        self.update_state(
+            state=states.FAILURE,
+            meta=ex
+        )
+        raise Ignore()
