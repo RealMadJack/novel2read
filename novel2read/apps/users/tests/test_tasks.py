3e0dfa3e5cb4c27e4d48c52f022a6cce7781300f
Dmitry G <gupalo1108@gmail.com>
2019-04-15 10:19:47 +0300

    test_update_users_votes_task

--- novel2read/apps/users/tests/test_tasks.py
+++ novel2read/apps/users/tests/test_tasks.py
@@ -12,7 +12,16 @@ class UserTaskTest(TestCase):
         self.user_1 = User.objects.create_user(username='testuser-1', password='test-1')
         self.user.profile.premium = True
         self.user.save()
+        self.user_1.profile.votes = 1
+        self.user_1.save()
 
     def test_update_users_votes_task(self):
-        resp = update_users_votes.apply().get()
-        print(resp)
+        self.assertEqual(self.user.profile.votes, 3)
+        self.assertEqual(self.user_1.profile.votes, 1)
+        update_users_votes.apply().get()
+        user = User.objects.get(username='testuser')
+        user_1 = User.objects.get(username='testuser-1')
+        self.assertEqual(user.profile.votes, 6)
+        self.assertEqual(user.profile.premium, True)
+        self.assertEqual(user_1.profile.votes, 3)
+        self.assertEqual(user_1.profile.premium, False)
