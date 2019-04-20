c33a2ce7161ae7d428f2b706a0a8f735fbc40364
Dmitry G <gupalo1108@gmail.com>
2019-04-20 13:39:21 +0300

    book_scraper_initial_signal - refactoring

--- novel2read/apps/books/signals.py
+++ novel2read/apps/books/signals.py
@@ -1,7 +1,10 @@
+import json
 from django.db import transaction
 from django.db.models import F
 from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
+from django.utils import timezone
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
 from .models import Book, BookChapter
 from .tasks import book_scraper_info, book_scraper_chaps
@@ -10,14 +13,29 @@ from .tasks import book_scraper_info, book_scraper_chaps
 @receiver(post_save, sender=Book)
 def book_scraper_initial_signal(sender, instance, created=False, **kwargs):
     if not instance.visited and instance.visit_id:
-        transaction.on_commit(
-            lambda: book_scraper_info.apply_async(args=(instance.pk,))
+        schedule, created = IntervalSchedule.objects.get_or_create(
+            every=10,
+            period=IntervalSchedule.SECONDS,
+        )
+        PeriodicTask.objects.create(
+            one_off=True,
+            interval=schedule,
+            name=f'Update book: {instance.title}',
+            task='novel2read.apps.books.tasks.book_scraper_info',
+            args=json.dumps([instance.pk]),
         )
 
-    if created:
-        transaction.on_commit(
-            lambda: book_scraper_chaps.apply_async(args=(instance.pk,))
-        )
+        if not instance.chapters_count:
+            PeriodicTask.objects.create(
+                one_off=True,
+                interval=schedule,
+                name=f'Update book chapters: {instance.title}',
+                task='novel2read.apps.books.tasks.book_scraper_chaps',
+                args=json.dumps([instance.pk]),
+                # kwargs=json.dumps({
+                #     's_to': 5,
+                # }),
+            )
 
 
 @receiver(post_save, sender=BookChapter)
