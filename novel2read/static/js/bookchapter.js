b3a7ae2b7decc49a5a259c8428ca8de64a3d2f8a
Dmitry G <gupalo1108@gmail.com>
2019-04-22 18:26:56 +0300

    bookchapters urls

--- novel2read/static/js/bookchapter.js
+++ novel2read/static/js/bookchapter.js
@@ -32,27 +32,27 @@ document.body.addEventListener('click', closeElem)
 
 
 // Scroll Position
-// let pos = localStorage.getItem('chap_scroll', 0)
-// let pos_url = localStorage.getItem('chap_url', '')
-// let posList = localStorage.getItem('pos-list')
-// let list = []
-// posList = posList !== null ? posList : localStorage.setItem('pos-list', list)
-// console.log(localStorage)
+let pos = localStorage.getItem('chap_scroll', 0)
+let pos_url = localStorage.getItem('chap_url', '')
+let posList = localStorage.getItem('pos-list')
+let list = []
+posList = posList !== null ? posList : localStorage.setItem('pos-list', list)
+console.log(localStorage)
 
-// window.onscroll = () => {
-//   const offset = 100;
-//   let scrollTop = window.pageYOffset;
-//   localStorage.setItem('chap_scroll', scrollTop);
-//   localStorage.setItem('chap_url', location.pathname)
-// }
+window.onscroll = () => {
+  const offset = 100;
+  let scrollTop = window.pageYOffset;
+  localStorage.setItem('chap_scroll', scrollTop);
+  localStorage.setItem('chap_url', location.pathname)
+}
 
-// if (pos_url === location.pathname) {
-//   window.scrollTo({
-//     top: pos,
-//     left: 0,
-//     behavior: 'smooth'
-//   });
-// }
+if (pos_url === location.pathname) {
+  window.scrollTo({
+    top: pos,
+    left: 0,
+    behavior: 'auto'
+  });
+}
 
 
 // Theming
