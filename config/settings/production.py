c42faed69a15fbe3d7d12df2cdf003dfa14bfade
Dmitry Gupalo <gupalo1108@gmail.com>
2019-05-18 19:13:46 +0000

    removed www_prepend

--- config/settings/production.py
+++ config/settings/production.py
@@ -17,8 +17,6 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['novel2read.com'])
 
-PREPEND_WWW = True
-
 # DATABASES
 # ------------------------------------------------------------------------------
 DATABASES['default'] = env.db('DATABASE_URL')  # noqa F405
